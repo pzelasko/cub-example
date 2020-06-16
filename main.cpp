1664eddd7bde7eb1cdd276c4004d4baec595fb5b
Piotr Żelasko <petezor@gmail.com>
2020-06-16 09:47:03 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "cub/util_allocator.cuh"
+
+cub::CachingDeviceAllocator  g_allocator(true);
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
